767bc5af88d11deb5a43b1e97ec088e3dd7a4356
sony <sony@son>
2019-04-29 07:49:40 +0530

    hey anish

--- src/main/java/main/SampleProject.java
+++ src/main/java/main/SampleProject.java
@@ -4,7 +4,7 @@ public class SampleProject {
 	//print  Hello world! in the console
 	public static String printHello() {
 		//print Hello world! in the console!
-		String c = "Hello World!";
+		String c = "Hello World";
 		return c;
 	}
 	public static int add(int a,int b) {
